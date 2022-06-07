fecf45d9d5b366c4a4d2a300ed92c3c6394d1dd0
f-1ej <89574401+f-1ej@users.noreply.github.com>
2022-06-07 19:32:47 +0800

    我的云村

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,27 +1,19 @@
 <template>
-	<view class="content">
-		{{title}}
+	<view>
+		
 	</view>
 </template>
+
 <script>
-	import { test200 } from "@/apis/test.js"
 	export default {
-	    data() {
-	        return {
-	            title: ''
-	        }
-	    },
-	    onLoad() {
-	        this.test()
-	    },
-	    methods: {
-	        // 测试获取数据
-	        test () {
-	            test200().then(res => {
-	                this.title = res.data
-	            })
-	        }
-	    }
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
 	}
 </script>
 
